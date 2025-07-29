432dcf971439ea741caa79ba537353c0b82ebb10
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-07-29 11:57:25 +0500

    commit

--- DAY 29 (Guess game Project)/script.js
+++ DAY 29 (Guess game Project)/script.js
@@ -5,6 +5,11 @@ let submit = document.querySelector("#submit-guess"); //targets submit button
 let triesSpan = document.querySelector("#tries"); // return number of tries
 let scoreSpan = document.querySelector("#score"); // gives current score
 let reset = document.querySelector("#reset-game") // targets reset button
+let openPopup = document.querySelector("#open-popup") // targets open popup button
+let closePopup = document.querySelector("#close-popup"); // targets close popup button
+let popup = document.querySelector("#popup"); // targets the popup div
+let overlay = document.querySelector("#overlay"); // targets the overlay div
+
 
 let tries = 0; // initialize tries
 let score = 0; // initialize score
@@ -43,4 +48,31 @@ reset.addEventListener("click", function()
     scoreSpan.textContent = score; // update the score
     document.querySelector("#guess").value = ""; // clear the input field
     document.querySelector("#guess").focus(); // set cursor back to input
+})
+
+openPopup.addEventListener("click", function()
+{
+    popup.style.display = "block"; // show the popup
+    overlay.style.display = "block"; // show the overlay
+})
+
+
+const closeOverlay= () =>
+{
+    popup.style.display = "none"; // hide the popup
+    overlay.style.display = "none"; // hide the overlay
+}
+
+// linking the above function with the button
+closePopup.addEventListener("click", closeOverlay);
+overlay.addEventListener("click", closeOverlay);
+
+
+// giving the close popup button a keyboard shortcut
+document.addEventListener("keydown", function(event)
+{
+    if(event.key ==="Escape" && popup.style.display === "block")
+    {
+        closeOverlay(); // call the function to close the popup
+    }
 })
