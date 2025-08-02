616e21b8780c71172e7ae5c9e7b1fff7550fbde3
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-08-02 14:13:18 +0500

    commit

--- DAY 34/shallow_deep.js
+++ DAY 34/shallow_deep.js
@@ -48,3 +48,31 @@ copy2.address.postalcode = 10000
 console.log(object2.address.postalcode); // now changing one dosnet effect other as they are not shared anymore but different copies
 console.log(copy2.address.postalcode);
 
+// 4. Garbage Collection (Exploration)
+// Simulate a scenario where large objects are created inside a function.
+// one using an arrow function
+const arrow =()=>
+{
+    var large =
+    {
+        data : new Array(100).fill("brain") // creates array with 100 slot and fill it with "brain"
+
+    }
+
+    console.log("📦 Large object created inside function");
+
+
+    console.log("🔍 Accessing one item:", large.data[0]);
+
+    console.log("⏹️ Function about to end (object still in memory for now)");
+}
+
+arrow()
+arrow()
+arrow()
+// after this the it is vaible for garbage colleciton
+
+// Explain (in comments) how they are removed from memory after the function finishes
+// (assuming no external reference remains).
+// Add console statements showing function calls and variable scopes to visualize memory
+// lifecycle.
