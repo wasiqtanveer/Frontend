0ac745d95b4096ecfbc7360da17b412e61008fea
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-08-07 11:57:51 +0500

    commit

---
+++ DAY 38/string.js
@@ -0,0 +1,41 @@
+// strings
+console.log("this is a string");
+
+// accessing strings
+let str = "wasiq"
+
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
+
+//template literals
+console.log(`This is a template literal ${str}`);
+
+//escape sequence
+console.log("Th\ris is an escape\n sequence\t exmaple");
+// \r is used to ermoce all the text before it
+// \n is used to start a new line
+// \t give a space of 4 spaces
+
+
+
+// Strings methods
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(1,4));
+console.log(str.slice(2));
+console.log(str.replace("was" , "777"));
+
+let b = "tanveer"
+console.log(str.concat(b));
+
+
+
+
+
+
+
+
