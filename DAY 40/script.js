53804a5035f3b3abefa744d545e530ac4ca799a4
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-08-07 13:19:01 +0500

    commit

---
+++ DAY 40/script.js
@@ -0,0 +1,25 @@
+//using call
+
+let obj1 = 
+{
+    name:"wasiq",
+    age : 23,
+    occupation : "student",
+    printdetails: function()
+    {
+        console.log(this);
+        
+    }
+}
+
+
+obj1.printdetails()
+
+let obj2 = 
+{
+    name:"wasiq",
+    age : 23,
+    occupation : "student",
+}
+
+obj2.prin
