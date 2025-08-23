120b77609d2520ca3b90dd8290b24cde428b06ee
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-08-23 12:03:46 +0500

    commit

---
+++ JavaScript/DAY 53/script.js
@@ -0,0 +1,25 @@
+let country = "pakistan";
+let url = `https://restcountrie.com/v3.1/name/${country}`;
+fetch(url)
+.then(response => response.json())
+.then(data => 
+{
+    console.log(data[0]);
+    console.log(`Capital : ${data[0].capital}`);
+    console.log(`Currency : ${data[0].currencies.EUR.name}`);
+    console.log(`Population : ${data[0].population}`);
+    console.log(`Flag Url : ${data[0].flags.png}`);
+    
+
+
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+        <h2>${data[0].name.common}</h2>
+        <p>Capital: ${data[0].capital}</p>
+        <p>Currency: ${data[0].currencies.EUR.name}</p>
+        <p>Population: ${data[0].population}</p>
+        <img src="${data[0].flags.png}" alt="Flag of ${data[0].name.common}">
+    `;
+    document.body.appendChild(newDiv);
+}
+);
