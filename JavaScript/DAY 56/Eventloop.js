fc3c5be7062481f1a2b6307c8016eaa75de940c8
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-09-04 14:53:50 +0500

    commit

--- JavaScript/DAY 56/Eventloop.js
+++ JavaScript/DAY 56/Eventloop.js
@@ -8,4 +8,22 @@ setTimeout(() =>
     console.log('setTimeout');
 },0)
 
+// promise (resolve only)
+new Promise((resolve) => {
+    resolve("promise Resolved");
+}).then((value) => console.log(value));
 
+
+// synchronous log
+console.log("start 2");
+
+
+
+
+// setTimeout will print at the end as even though its time is 0ms, it has to wait for the current execution stack to be empty
+// promise will print before setTimeout as it goes to microtask queue which has higher priority than macrotask queue (setTimeout)
+// so the order will be:
+// Start
+// start 2
+// promise Resolved
+// setTimeout
