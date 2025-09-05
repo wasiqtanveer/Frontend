3d6bcb780a2b3970698db6f02c3feb4c55368bf5
Muhammad Wasiq Tanveer <149468602+wasiqtanveer@users.noreply.github.com>
2025-09-05 22:05:57 +0500

    commit

--- JavaScript/DAY 59 (Final Project)/script.js
+++ JavaScript/DAY 59 (Final Project)/script.js
@@ -1,2 +1,34 @@
+let search = document.getElementById("search");
+let input = document.getElementById("input");
+let dishName = document.getElementById("dish-name");
+let category = document.getElementById("category");
+let dishImage = document.getElementById("dish-image");
+let instructions = document.getElementById("instructions");
+
+let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Chicken Handi`
+
+// async that dictates what happens on api call
+async function apiCall()
+{
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log(data.meals[0].strCategory);
+    console.log(data.meals[0].strInstructions);
+    console.log(data.meals[0].strMealThumb);
+    console.log(data.meals);
+    dishName.textContent = data.meals[0].strMeal
+    category.textContent = data.meals[0].strCategory
+    dishImage.src = data.meals[0].strMealThumb
+    instructions.textContent = data.meals[0].strInstructions
+
+    
+}
+
+apiCall()
+
+search.addEventListener('click',() => 
+{
+
+});
 
 
